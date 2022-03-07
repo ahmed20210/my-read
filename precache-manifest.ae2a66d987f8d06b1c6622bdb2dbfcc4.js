d8cf6a8d241cbcff8ed83de54ebeb63985faad1d
ahmed20210 <85214621+ahmed20210@users.noreply.github.com>
2022-03-07 21:31:23 +0200

    Updates

---
+++ precache-manifest.ae2a66d987f8d06b1c6622bdb2dbfcc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "59cb2e8313c56e6828b5",
+    "url": "/my-read/static/css/main.a762df86.chunk.css"
+  },
+  {
+    "revision": "59cb2e8313c56e6828b5",
+    "url": "/my-read/static/js/main.59cb2e83.chunk.js"
+  },
+  {
+    "revision": "718990f722b0ff1d14bb",
+    "url": "/my-read/static/js/1.718990f7.chunk.js"
+  },
+  {
+    "revision": "00eb463b94da3db23a11",
+    "url": "/my-read/static/js/runtime~main.00eb463b.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-read/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-read/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-read/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "c6e56edab62eea944ac08351f7831835",
+    "url": "/my-read/index.html"
+  }
+];
